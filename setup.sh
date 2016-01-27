f5ed7298fa3a268c869f137fc23cb48afd03f05a
Edward Lu <lued@microsoft.com>
2016-01-27 15:46:58 -0800

    Setup colors

--- setup.sh
+++ setup.sh
@@ -6,3 +6,14 @@ ln -sn $PWD/xmonad $HOME/.xmonad
 ln -s $PWD/Xresources $HOME/.Xresources
 ln -s $PWD/xmobarrc $HOME/.xmobarrc
 ln -s $PWD/vimrc $HOME/.vimrc
+
+if [ `uname -o` = "Cygwin" ]; then 
+    curl -LSso $HOME/.minttyrc https://raw.githubusercontent.com/mavnn/mintty-colors-solarized/master/.minttyrc.dark
+fi
+
+mkdir -p $HOME/.vim/autoload/
+curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p $HOME/.vim/bundle/
+cd $HOME/.vim/bundle/
+git clone https://github.com/altercation/vim-colors-solarized.git
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
